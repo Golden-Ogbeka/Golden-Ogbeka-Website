5e677f6d9840a93b67f71a1999e583d0b4f3f594
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2022-09-03 13:54:39 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	env: {
-		API_URL: 'https://api-dev-frscsm-lagos.azurewebsites.net/api/v1',
-		SESSION_KEY: 'FRSC_SPECIAL_MARSHALL',
-		SESSION_NAME: 'FRSC_SM',
+		
 	},
 	eslint: {
 		ignoreDuringBuilds: true,
