0ffdc6c362bca2fb0d53f39a141509d6250b9e7d
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2023-05-17 15:12:26 +0100

    change default view mode to light

--- app/slices/darkModeSlice.ts
+++ app/slices/darkModeSlice.ts
@@ -2,19 +2,19 @@ import { createSlice } from '@reduxjs/toolkit';
 
 // Actual Slice
 export const darkModeSlice = createSlice({
-	name: 'darkMode',
-	initialState: {
-		isDark: true,
-	},
-	reducers: {
-		toggleDarkMode(state) {
-			state.isDark = !state.isDark;
-			localStorage.setItem('theme', state.isDark ? 'dark' : 'light');
-		},
-		setDarkMode(state, action) {
-			state.isDark = action.payload;
-		},
-	},
+  name: 'darkMode',
+  initialState: {
+    isDark: false,
+  },
+  reducers: {
+    toggleDarkMode(state) {
+      state.isDark = !state.isDark;
+      localStorage.setItem('theme', state.isDark ? 'dark' : 'light');
+    },
+    setDarkMode(state, action) {
+      state.isDark = action.payload;
+    },
+  },
 });
 
 export const { toggleDarkMode, setDarkMode } = darkModeSlice.actions;
