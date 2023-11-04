ffcb4c0863e466cb05ef10b298e272369f19ebce
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2023-11-04 13:03:12 +0100

    Added google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,34 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
-	return (
-		<Html>
-			<Head>
-				{/* <!-- Fonts --> */}
-				<link rel='preconnect' href='https://fonts.googleapis.com' />
-				<link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
-				<link
-					href='https://fonts.googleapis.com/css2?family=Inter:wght@200;300&family=Montserrat:wght@700&display=swap'
-					rel='stylesheet'
-				/>
-			</Head>
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+  return (
+    <Html>
+      <Head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script async src='https://www.googletagmanager.com/gtag/js?id=G-NZGV1R34HY' />
+        <Script id='google-analytics'>
+          {`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+
+						gtag('config', 'G-NZGV1R34HY');
+        `}
+        </Script>
+
+        {/* <!-- Fonts --> */}
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Inter:wght@200;300&family=Montserrat:wght@700&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
