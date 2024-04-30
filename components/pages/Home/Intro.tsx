bbd2573739cbd5f00a994b7acc225bf03261a20f
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2024-04-30 22:22:46 +0100

    into update

--- components/pages/Home/Intro.tsx
+++ components/pages/Home/Intro.tsx
@@ -1,9 +1,8 @@
 import Image from 'next/image';
-import React from 'react';
-import GithubIcon from '../../../public/icons/github.png';
-import LinkedinIcon from '../../../public/icons/linkedin.png';
 import DribbbleIcon from '../../../public/icons/dribbble.png';
 import EmailIcon from '../../../public/icons/email.png';
+import GithubIcon from '../../../public/icons/github.png';
+import LinkedinIcon from '../../../public/icons/linkedin.png';
 import WhatsappIcon from '../../../public/icons/whatsapp.png';
 
 function Intro() {
@@ -14,9 +13,9 @@ function Intro() {
         Golden Ogbeka
       </h1>
       <p className='text-lg font-extralight dark:text-[#F0F0F0] mb-10'>
-        I&apos;m a frontend web developer based in Lagos, Nigeria. I&apos;m passionate
-        about using technology to create solutions for people and have experience doing so
-        in these sectors: Agriculture, Finance, Real Estate, Retail Services, IT, Food and
+        I&apos;m a software engineer based in Lagos, Nigeria. I&apos;m passionate about
+        using technology to create solutions for people and have experience doing so in
+        these sectors: Agriculture, Finance, Real Estate, Retail Services, IT, Food and
         Logistics.
       </p>
       <div className='flex flex-row gap-5 items-center flex-wrap'>
