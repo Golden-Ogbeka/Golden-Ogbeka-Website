08fd14ce3205c2c591f042edbca84e9c899b7861
golden-rova <ogbekagolden@gmail.com>
2026-06-02 11:49:47 +0100

    updated resume

--- components/layout/Navbar/Navbar.tsx
+++ components/layout/Navbar/Navbar.tsx
@@ -12,7 +12,10 @@ function Navbar() {
   const { isDark } = useAppSelector((state) => state.darkMode);
 
   return (
-    <nav aria-label='Main navigation' className='py-6 px-4 sm:px-6 lg:px-8 w-full max-w-screen-2xl mx-auto flex flex-row items-center justify-between'>
+    <nav
+      aria-label='Main navigation'
+      className='py-6 px-4 sm:px-6 lg:px-8 w-full max-w-screen-2xl mx-auto flex flex-row items-center justify-between'
+    >
       <Link href='/'>
         <a className='flex-shrink-0 flex items-center gap-3'>
           <Image
@@ -45,7 +48,7 @@ function Navbar() {
           </a>
         </Link>
         <a
-          href='/brand/Resume-Golden-Ogbeka.pdf'
+          href='/brand/Golden-Ogbeka-Resume.pdf'
           target='_blank'
           rel='noopener noreferrer'
           download='Golden_Ogbeka_Resume'
